a141e535503dbeea3eadc25f35444aaedfaeb868
pashq96 <39460505+pashq96@users.noreply.github.com>
2018-11-14 22:00:35 +0300

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+  $('.single-item').slick({
+	dots: true,
+	prevArrow:"<img class='a-left control-c prev slick-prev' style = 'width: 19px; height: 81px;' src='img/prev.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' style = 'width: 19px; height: 81px;' src='img/next.png'>"
+});
+});
